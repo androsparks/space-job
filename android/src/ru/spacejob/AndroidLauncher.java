86c1441690268d2fb01472c6841346c6a406b33a
SPunkY <SPunkY-1992@yandex.ru>
2017-09-07 17:20:37 +0300

    just created project

---
+++ android/src/ru/spacejob/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.spacejob;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.spacejob.SpaceJob;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpaceJob(), config);
+	}
+}
